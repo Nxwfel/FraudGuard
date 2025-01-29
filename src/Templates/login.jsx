7724d3075af6d034f56f14f5bd94b0a9d64621a0
Nxwfel <nelxpe@gmail.com>
2025-01-29 15:41:39 +0000

    Updated files

--- src/Templates/login.jsx
+++ src/Templates/login.jsx
@@ -1,46 +1,37 @@
+import React from 'react';
 import TextPressure from '../Animation/TextPressure';
-import Shield from '../Images/encrypted.png';
-import '../Style/login.css'
+import '../Style/login.css';
+
 const Login = () => {
-
-
   return (
     <div className='flex h-screen w-screen'>
-      <div className='h-screen w-2/3 bg-gradient-to-b from-cyan-500 to-sky-950'>
-      <div style={{position: 'relative', height: '300px', top: '30%'}}>
-        <TextPressure
-          text="FraudGuard"
-          flex={true}
-          alpha={false}
-          stroke={false}
-          width={true}
-          weight={true}
-          italic={true}
-          textColor="#ffffff"
-          strokeColor="#ff0000"
-          minFontSize={36}
-        />
+      <div className='h-screen w-screen items-center justify-center bg-gradient-to-b from-cyan-500 to-sky-950 '>
+        <div style={{ position: 'absolute', height: '300px', width: "100%", top: '30%' }}>
+          <TextPressure
+            text="FraudGuard"
+            flex={true}
+            alpha={false}
+            stroke={false}
+            width={true}
+            weight={true}
+            italic={true}
+            textColor="#ffffff"
+            strokeColor="#ff0000"
+            minFontSize={36}
+          />
+        </div>
+        <div className='h-3/5 w-96 mt-28 relative mr-auto ml-auto rounded-xl bg-white'>
+          <h1 className='font-textfont2 text-4xl text-sky-400 text-center'>Login</h1>
+          <div className="form__group field">
+            <input type="input" className="form__field" placeholder="Name" required="" />
+            <label htmlFor="name" className="form__label">Name</label>
+          </div>
+            <div className="form__group field">
+              <input type="input" className="form__field" placeholder="Name" required="" />
+              <label htmlFor="name" className="form__label">Name</label>
+            </div>
+        </div>
       </div>
-      <div className='flex justify-center relative mt-20'>
-        <img src={Shield} alt='shield' className='h-48 w-48'/></div>
-      </div>
-      <div className='h-screen w-1/3 bg-white '>
-      <div className='flex justify-center items-center flex-col min-w-full'>
-      <div class="group">
-  <input required="" type="text" class="input" />
-  <span class="highlight"></span>
-  <span class="bar"></span>
-  <label>Name</label>
-</div>
-<div class="group">
-  <input required="" type="text" class="input" />
-  <span class="highlight"></span>
-  <span class="bar"></span>
-  <label>Name</label>
-</div>
-      </div>
-      </div>
-      
     </div>
   );
 };
